661fc2f9cae56d0c1df6cd7d0263777f054f2839
mohamed omara <134385233+horees101@users.noreply.github.com>
2026-01-06 19:25:25 +0200

    Fix console dialog script compatibility

--- bench_manager/public/js/bench_manager.js
+++ bench_manager/public/js/bench_manager.js
@@ -4,25 +4,25 @@
 (function () {
 	"use strict";
 
-	const update_console = (state) => {
+	function update_console(state) {
 		if (!state.in_progress) {
 			state.target.innerHTML = state.output;
 		}
-	};
+	}
 
 	window.console_dialog = function (key) {
-		const dialog = new frappe.ui.Dialog({
+		var dialog = new frappe.ui.Dialog({
 			title: __("Console"),
 			fields: [{ fieldname: "console", fieldtype: "HTML" }],
 		});
 
-		const target = $('<pre class="console"><code></code></pre>')
+		var target = $('<pre class="console"><code></code></pre>')
 			.appendTo(dialog.get_field("console").wrapper)
 			.find("code")
 			.get(0);
 
-		const state = {
-			target,
+		var state = {
+			target: target,
 			output: "",
 			in_progress: false,
 			last_update: null,
@@ -32,7 +32,7 @@
 		dialog.show();
 		dialog.$wrapper.find(".modal-dialog").css("width", "800px");
 
-		frappe.realtime.on(key, (output) => {
+		frappe.realtime.on(key, function (output) {
 			if (output === "\r") {
 				state.in_progress = true;
 				update_console(state);
@@ -51,7 +51,7 @@
 			}
 
 			if (!state.last_update) {
-				state.last_update = setTimeout(() => {
+				state.last_update = setTimeout(function () {
 					state.last_update = null;
 					update_console(state);
 				}, 200);
